fd751ee2ffceade8c0aa3ccdf2398181b1a81bdb
Geert Ijewski <gm.ijewski@web.de>
2026-02-10 22:39:06 +0200

    fix compiling on msvc

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -44,14 +44,13 @@ function(
       endif()
     endif()
   elseif(MSVC)
-    if(${ENABLE_SANITIZER_ADDRESS})
-      list(APPEND SANITIZERS "address")
-    endif()
-    if(${ENABLE_SANITIZER_LEAK}
+    # Disable sanitizers for MSVC builds.Always gave warnings that it doesn't work with /MDd
+    if(${ENABLE_SANITIZER_ADDRESS}
+       OR ${ENABLE_SANITIZER_LEAK}
        OR ${ENABLE_SANITIZER_UNDEFINED_BEHAVIOR}
        OR ${ENABLE_SANITIZER_THREAD}
        OR ${ENABLE_SANITIZER_MEMORY})
-      message(WARNING "MSVC only supports address sanitizer")
+      message(WARNING "Sanitizers are disabled for MSVC builds; address sanitizer will not be enabled on MSVC.")
     endif()
   endif()
 
